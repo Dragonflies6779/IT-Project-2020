951f20dd996f57fec9502e70825b7559862fd968
Dragonflies6779 <Dragonflies6779@gmail.com>
2020-09-07 00:00:46 +1000

    Changed the css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 import LoginForm from './LoginForm';
 import InputField from './InputField';
 import SubmitButton form './SubmitButton';
