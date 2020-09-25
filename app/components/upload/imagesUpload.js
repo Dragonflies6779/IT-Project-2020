b2fdb1363e39acf9eecaf15749b598c1ec4bb6a9
Anthony <Dragonflies6779@gmail.com>
2020-09-25 23:58:48 +1000

    Chnaged file name Added the way to upload a file

---
+++ app/components/upload/imagesUpload.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+class imagesUpload extends React.Component{
+
+  state = {
+    selectedFile: null
+  };
+
+  onFileChange = event => {
+    this.setState({ selectedFile: event.target.files[0]});
+  };
+
+  onFileUpload = ()) => {
+
+    //What needs to be here is the path where to sore it
+    var storageRef = firebase.storage().ref();
+    var mountainsRef = storageRef.child('mountains.jpg');
+    var mountainImagesRef = storageRef.child('images/mountains.jpg');
+
+  }
+
+  fileData = () => {
+
+      if (this.state.selectedFile) {
+
+        return (
+          <div>
+            <h2>File Details:</h2>
+            <p>File Name: {this.state.selectedFile.name}</p>
+            <p>File Type: {this.state.selectedFile.type}</p>
+            <p>
+              Last Modified:{" "}
+              {this.state.selectedFile.lastModifiedDate.toDateString()}
+            </p>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <br />
+            <h4>Choose before Pressing the Upload button</h4>
+          </div>
+        );
+      }
+    };
+
+    render() {
+
+      return (
+        <div>
+            <div>
+                <input type="file" onChange={this.onFileChange} />
+                <button onClick={this.onFileUpload}>
+                  Upload
+                </button>
+            </div>
+          {this.fileData()}
+        </div>
+      );
+    }
+  }
